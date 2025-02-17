d8485d4152be0c547762bcfd81993b2b297f2d6f
Yuri Silva <yuriteofilo38@gmail.com>
2025-02-16 23:18:40 -0300

    Ajustes process

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,9 @@
 // Fetch ou Axios
 import axios from "axios";
-import "dotenv/config.js";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const { NODE_ENV } = process.env;
+//const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://projeto-full-stack-6158.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
